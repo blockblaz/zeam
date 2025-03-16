8141b69271789ae0bc42ef22617a58dd23a0d48e
harkamal <gajinder@zeam.in>
2025-03-16 17:53:41 +0530

    add subcomand to run just clock

--- pkgs/cli/src/main.zig
+++ pkgs/cli/src/main.zig
@@ -8,6 +8,8 @@ const Clock = nodeLib.Clock;
 const ZeamArgs = struct {
     genesis: ?u64,
 
+    __commands__: union(enum) { clock: struct {} },
+
     pub const __messages__ = .{
         .genesis = "genesis time",
     };
@@ -19,9 +21,13 @@ pub fn main() !void {
     const genesis = opts.args.genesis orelse 1234;
     std.debug.print("opts ={any} genesis={d}\n", .{ opts, genesis });
 
-    var clock = try Clock.init(genesis);
-    std.debug.print("clock {any}\n", .{clock});
+    switch (opts.args.__commands__) {
+        .clock => {
+            var clock = try Clock.init(genesis);
+            std.debug.print("clock {any}\n", .{clock});
 
-    clock.start();
-    try clock.run();
+            clock.start();
+            try clock.run();
+        },
+    }
 }
