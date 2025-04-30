56ee2fd921e3f9a64552bd83565242df1874f176
harkamal <gajinder@zeam.in>
2025-04-30 21:35:25 +0530

    restore cast

--- pkgs/utils/src/cast.zig
+++ pkgs/utils/src/cast.zig
@@ -3,12 +3,10 @@ const std = @import("std");
 pub fn Cast(To: type, from: anytype) To {
     var result: To = undefined;
     inline for (@typeInfo(To).@"struct".fields) |field| {
-        const cast_value = @field(from, field.name);
-
         const field_value = switch (@typeInfo(field.type)) {
-            .optional => cast_value,
+            .optional => @field(from, field.name),
             // TODO: throw error instead of panic?
-            else => cast_value orelse @panic("optional value for non optional field"),
+            else => @field(from, field.name) orelse @panic("optional value for non optional field"),
         };
 
         @field(result, field.name) = field_value;
