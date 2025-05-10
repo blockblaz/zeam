baa40b5706b7c376cabf787ba44f86dabe869560
harkamal <gajinder@zeam.in>
2025-05-10 13:57:19 +0530

    remove extend for now

--- pkgs/utils/src/lib.zig
+++ pkgs/utils/src/lib.zig
@@ -6,6 +6,3 @@ pub usingnamespace mixFactory;
 
 const castFactory = @import("./cast.zig");
 pub usingnamespace castFactory;
-
-const extendFactory = @import("./extend.zig");
-pub usingnamespace extendFactory;
