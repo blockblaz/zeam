d52b4f0df359dc2fe5e3fecd9c4b02a340dd8dfd
harkamal <gajinder@zeam.in>
2025-05-14 22:28:04 +0530

    get 3sf stf code and build working

--- pkgs/state-transition/src/transition.zig
+++ pkgs/state-transition/src/transition.zig
@@ -46,6 +46,27 @@ pub fn process_slots(allocator: Allocator, state: *types.BeamState, slot: types.
     }
 }
 
+fn is_justifiable_slot(finalized: types.Slot, candidate: types.Slot) !bool {
+    if (candidate < finalized) {
+        return StateTransitionError.InvalidJustifiableSlot;
+    }
+
+    const delta: f32 = @floatFromInt(candidate - finalized);
+    if (delta <= 5) {
+        return true;
+    }
+    const delta_x2: f32 = @mod(std.math.pow(f32, delta, 0.5), 1);
+    if (delta_x2 == 0) {
+        return true;
+    }
+    const delta_x2_x: f32 = @mod(std.math.pow(f32, delta + 0.25, 0.5), 1);
+    if (delta_x2_x == 0.5) {
+        return true;
+    }
+
+    return false;
+}
+
 fn process_block_header(allocator: Allocator, state: *types.BeamState, block: types.BeamBlock) !void {
     // very basic process block header
     if (state.slot != block.slot) {
@@ -70,10 +91,114 @@ fn process_execution_payload_header(state: *types.BeamState, block: types.BeamBl
     }
 }
 
+fn process_operations(allocator: Allocator, state: *types.BeamState, block: types.BeamBlock) !void {
+    // transform state data into consumable format, generally one would keep a `cached`/consumable
+    // copy of state but we will get to that later especially w.r.t. proving
+    // prep data
+    var historical_block_hashes = std.ArrayList(types.Root).fromOwnedSlice(allocator, state.historical_block_hashes);
+    var justified_slots = std.ArrayList(u8).fromOwnedSlice(allocator, state.justified_slots);
+    // prep the justifications map
+    var justifications = std.AutoHashMap(types.Root, []u8).init(allocator);
+
+    const num_validators = state.config.num_validators;
+    for (state.justifications_roots) |blockRoot| {
+        for (0..state.config.num_validators) |i| {
+            try justifications.put(blockRoot, state.justifications_validators[i * num_validators .. (i + 1) * num_validators]);
+        }
+    }
+
+    // 3sf
+    try historical_block_hashes.append(block.parent_root);
+    try justified_slots.append(0);
+    const missed_slots = block.slot - historical_block_hashes.items.len;
+    for (0..missed_slots) |i| {
+        _ = i;
+        try justified_slots.append(0);
+        // we push zero hash instead of none to keep our SSZ structure simple
+        // in applying votes we can eliminate this issue by having source/target to be non zerohash
+        // because genesis is always justified and finalized
+        try historical_block_hashes.append(utils.ZERO_HASH);
+    }
+
+    for (block.body.votes) |vote| {
+        // check if vote is sane
+        if (justified_slots.items[vote.source.slot] != 1 or
+            !std.mem.eql(u8, &vote.source.root, &historical_block_hashes.items[vote.source.slot]) or
+            !std.mem.eql(u8, &vote.target.root, &historical_block_hashes.items[vote.target.slot]) or
+            vote.target.slot <= vote.source.slot or
+            try is_justifiable_slot(state.lastest_finalized.slot, vote.target.slot) == false)
+        {
+            continue;
+        }
+        if (vote.validator_id >= num_validators) {
+            return StateTransitionError.InvalidValidatorId;
+        }
+
+        var target_justifications = justifications.get(vote.target.root) orelse targetjustifications: {
+            var targetjustifications = try allocator.alloc(u8, num_validators);
+            for (0..targetjustifications.len) |i| {
+                targetjustifications[i] = 0;
+            }
+            try justifications.put(vote.target.root, targetjustifications);
+            break :targetjustifications targetjustifications;
+        };
+
+        target_justifications[vote.validator_id] = 1;
+        var target_justifications_count: usize = 0;
+        for (target_justifications) |justified| {
+            if (justified == 1) {
+                target_justifications_count += 1;
+            }
+        }
+
+        // as soon as we hit the threshold do justifications
+        // note that this simplification works if weight of each validator is 1
+        if (target_justifications_count == @divFloor(num_validators * 2, 3)) {
+            state.latest_justified = vote.target;
+            justified_slots.items[vote.target.slot] = 1;
+            _ = justifications.remove(vote.target.root);
+        }
+
+        // source is finalized if target is the next valid justifiable hash
+        var can_target_finalize = true;
+        for (vote.source.slot + 1..vote.target.slot) |check_slot| {
+            if (try is_justifiable_slot(state.lastest_finalized.slot, check_slot)) {
+                can_target_finalize = false;
+                break;
+            }
+        }
+        if (can_target_finalize == true) {
+            state.lastest_finalized = vote.source;
+        }
+    }
+
+    // reconstiture back the state vectors
+    state.historical_block_hashes = try historical_block_hashes.toOwnedSlice();
+    state.justified_slots = try justified_slots.toOwnedSlice();
+
+    var justifications_roots = std.ArrayList(types.Root).init(allocator);
+    var justifications_validators = std.ArrayList(u8).init(allocator);
+    var iterator = justifications.iterator();
+    while (iterator.next()) |kv| {
+        try justifications_roots.append(kv.key_ptr.*);
+        try justifications_validators.appendSlice(kv.value_ptr.*);
+    }
+
+    allocator.free(state.justifications_roots);
+    allocator.free(state.justifications_validators);
+    state.justifications_roots = try justifications_roots.toOwnedSlice();
+    state.justifications_validators = try justifications_validators.toOwnedSlice();
+
+    for (state.justifications_roots) |root| {
+        _ = justifications.remove(root);
+    }
+}
+
 pub fn process_block(allocator: Allocator, state: *types.BeamState, block: types.BeamBlock) !void {
     // start block processing
     try process_block_header(allocator, state, block);
     try process_execution_payload_header(state, block);
+    try process_operations(allocator, state, block);
 }
 
 // fill this up when we have signature scheme
@@ -117,4 +242,6 @@ pub const StateTransitionError = error{
     InvalidPreState,
     InvalidPostState,
     InvalidExecutionPayloadHeaderTimestamp,
+    InvalidJustifiableSlot,
+    InvalidValidatorId,
 };
