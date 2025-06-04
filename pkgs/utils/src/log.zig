8fd8a918f0caa6036b4509806b07b3f5e4003356
harkamal <gajinder@zeam.in>
2025-06-04 19:56:26 +0530

    do not throw error on log because its somehow messing with noop log object f

--- pkgs/utils/src/log.zig
+++ pkgs/utils/src/log.zig
@@ -3,7 +3,7 @@ const builtin = @import("builtin");
 
 // having activeLevel non comptime and dynamic allows us env based logging and even a keystroke activated one
 // on a running client, may be can be revised later
-pub fn log(comptime scope: @Type(.enum_literal), activeLevel: std.log.Level, comptime level: std.log.Level, comptime fmt: []const u8, args: anytype) !void {
+pub fn log(comptime scope: @Type(.enum_literal), activeLevel: std.log.Level, comptime level: std.log.Level, comptime fmt: []const u8, args: anytype) void {
     if (@intFromEnum(level) > @intFromEnum(activeLevel)) {
         return;
     }
@@ -18,8 +18,12 @@ pub fn log(comptime scope: @Type(.enum_literal), activeLevel: std.log.Level, com
 
     if (builtin.target.os.tag == .freestanding) {
         const io = @import("zkvm").io;
-        var buf: [512]u8 = undefined;
-        io.print_str(try std.fmt.bufPrint(buf[0..], prefix ++ fmt, args));
+        var buf: [1024]u8 = undefined;
+        // TODO don't throw error because it somehow messes with creation of  noopLogger as noopLog
+        // doesn't throw and somehow it can't seem to infer error types as they might not be same
+        // across all log fns, figure out in a later PR
+        const print_str = std.fmt.bufPrint(buf[0..], prefix ++ fmt, args) catch "error formatting log";
+        io.print_str(print_str);
     } else {
         std.debug.lockStdErr();
         defer std.debug.unlockStdErr();
@@ -31,7 +35,7 @@ pub fn log(comptime scope: @Type(.enum_literal), activeLevel: std.log.Level, com
 // just a handy debugging log used in the project
 pub fn zeamLog(comptime fmt: []const u8, args: anytype) !void {
     // forcing all logs for now
-    try log(std.log.default_log_scope, std.log.Level.debug, std.log.Level.debug, fmt, args);
+    log(std.log.default_log_scope, std.log.Level.debug, std.log.Level.debug, fmt, args);
 }
 
 pub const ZeamLogger = struct {
