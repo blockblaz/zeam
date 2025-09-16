da7092c6d5e2558881c6666a751ee9420323d8d3
ScottyPoi <scott.simpson@ethereum.org>
2025-09-16 16:50:51 -0600

    build tree visualization in printSlot function

--- pkgs/node/src/chain.zig
+++ pkgs/node/src/chain.zig
@@ -17,6 +17,7 @@ const zeam_utils = @import("@zeam/utils");
 
 pub const fcFactory = @import("./forkchoice.zig");
 const constants = @import("./constants.zig");
+const tree_visualizer = @import("./tree_visualizer.zig");
 
 pub const BlockProductionParams = struct {
     slot: usize,
@@ -223,6 +224,11 @@ pub const BeamChain = struct {
         const slot: usize = if (islot < 0) 0 else @intCast(islot);
         const blocks_behind = if (slot > fc_head.slot) slot - fc_head.slot else 0;
         const is_timely = fc_head.timeliness;
+
+        // Build tree visualization
+        var arena = std.heap.ArenaAllocator.init(self.allocator);
+        defer arena.deinit();
+        const tree_visual = tree_visualizer.buildTreeVisualization(arena.allocator(), self.forkChoice.protoArray.nodes.items) catch "Tree visualization failed";
 
         self.module_logger.info(
             \\
