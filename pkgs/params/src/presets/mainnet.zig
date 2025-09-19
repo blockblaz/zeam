79e9914048416532399f1cae22142b46a5d88b16
harkamal <gajinder@zeam.in>
2025-09-20 02:19:46 +0530

    increase limits till they start breaking

--- pkgs/params/src/presets/mainnet.zig
+++ pkgs/params/src/presets/mainnet.zig
@@ -7,7 +7,8 @@ pub const preset = types.PresetConfig{
     // reduced size of roots limit because of following build issue
     // caused by super big stack allocated bounded lists
     // error: warning: mock.zig:27:0: stack frame size (5002379528) exceeds limit (4294967295) in function 'mock.genMockChain'
-    .HISTORICAL_ROOTS_LIMIT = 1 << 4, // 2^18 = 262144
-    .VALIDATOR_REGISTRY_LIMIT = 1 << 8, // 2^12 = 4096
+    // even beam run command results into core dumped
+    .HISTORICAL_ROOTS_LIMIT = 1 << 10, // 2^18 = 262144
+    .VALIDATOR_REGISTRY_LIMIT = 1 << 4, // 2^12 = 4096
     .MAX_REQUEST_BLOCKS = 1024,
 };
