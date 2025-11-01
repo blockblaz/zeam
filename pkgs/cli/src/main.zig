1c6f244bae6db53c8bfb2d9b84dea65c8c322620
ch4r10t33r <psramanuj@gmail.com>
2025-11-01 22:46:36 +0530

    fix: fixed ci eror

--- pkgs/cli/src/main.zig
+++ pkgs/cli/src/main.zig
@@ -239,7 +239,7 @@ fn mainInner() !void {
             // starting beam state
             var beam_state = mock_chain.genesis_state;
             // block 0 is genesis so we have to apply block 1 onwards
-            for (mock_chain.blocks[1..]) |block| {
+            for (mock_chain.blocks[1..]) |signed_block| {
                 const block = signed_block.message.block;
                 std.debug.print("\nprestate slot blockslot={d} stateslot={d}\n", .{ block.slot, beam_state.slot });
                 const proof = state_proving_manager.prove_transition(beam_state, block, options, allocator) catch |err| {
