1a7d9e3da692c019a90b0290fd528d291a4877cb
harkamal <gajinder@zeam.in>
2025-11-28 00:49:54 +0530

    small print cleanup

--- pkgs/types/src/state.zig
+++ pkgs/types/src/state.zig
@@ -604,7 +604,7 @@ test "ssz seralize/deserialize signed beam state" {
     var serialized_state = std.ArrayList(u8).init(std.testing.allocator);
     defer serialized_state.deinit();
     try ssz.serialize(BeamState, state, &serialized_state);
-    std.debug.print("\n\n\nserialized_state ({d})", .{serialized_state.items.len});
+    std.debug.print("serialized_state ({d})\n", .{serialized_state.items.len});
 
     // we need to use arena allocator because deserialization allocs without providing for
     // a way to deinit, this needs to be probably addressed in ssz
