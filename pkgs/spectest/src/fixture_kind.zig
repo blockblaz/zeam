19fdd510626bfb3fe4b1ecf673a06b767d0553cf
Chen Kai <281165273grape@gmail.com>
2025-12-22 12:53:42 +0800

    feat: impl sig spec test
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- pkgs/spectest/src/fixture_kind.zig
+++ pkgs/spectest/src/fixture_kind.zig
@@ -1,11 +1,15 @@
 pub const FixtureKind = enum {
     state_transition,
     fork_choice,
+    // verify_signatures is temporarily disabled due to XMSS config mismatch
+    // between Python test config (424-byte signatures) and Rust production config (3112-byte signatures)
+    // verify_signatures,
 
     pub fn runnerModule(self: FixtureKind) []const u8 {
         return switch (self) {
             .state_transition => "state_transition",
             .fork_choice => "fork_choice",
+            // .verify_signatures => "verify_signatures",
         };
     }
 
@@ -13,6 +17,7 @@ pub const FixtureKind = enum {
         return switch (self) {
             .state_transition => "state_transition",
             .fork_choice => "fc",
+            // .verify_signatures => "verify_signatures",
         };
     }
 };
